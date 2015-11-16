8efd5b7363c8bf584e8388efc4ee885aa9e1506d
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2015-11-16 19:23:25 +0900

    add model import helper

---
+++ Editor/ModelImportHelper/ModelImportHelper.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class ModelImportHelper : Editor {
+
+	private static string objectNameToAlign = "Plane001";
+	private static string[] objectNamesToDelete = {"g_3dboard","Plane001"};
+
+	[MenuItem("Custom/Align Models")]
+	public static void AlignModels(){
+		Debug.Log ("Align Models");
+
+		Transform[] transformes = GameObject.FindObjectsOfType<Transform> ();
+		foreach (Transform trans in transformes) {
+			if(trans.name == ModelImportHelper.objectNameToAlign){
+				Vector3 pos = trans.position;
+				Transform root = trans.parent;
+				root.Translate(-pos);
+			}
+//			if(objectNamesToDelete.Contains(trans.name)){
+//				DestroyImmediate(trans.gameObject);
+//			}
+		}
+
+	}
+
+	[MenuItem("Custom/Add Mesh Collider")]
+	public static void AddMeshCollider(){
+		GameObject selected = Selection.activeGameObject;
+		MeshFilter[] mfs = selected.GetComponentsInChildren<MeshFilter> ();
+		foreach (MeshFilter mf in mfs) {
+			if(!mf.gameObject.GetComponent<Collider>())
+				mf.gameObject.AddComponent<MeshCollider>();
+		}
+	}
+
+	[MenuItem("Custom/Remove Collider")]
+	public static void RemoteCollider(){
+		GameObject selected = Selection.activeGameObject;
+		Collider[] mfs = selected.GetComponentsInChildren<Collider> ();
+		foreach (Collider mf in mfs) {
+			DestroyImmediate(mf);
+		}
+	}
+}
