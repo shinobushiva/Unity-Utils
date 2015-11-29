46a02b610ca97a8ed5ab01d57ca2d157f5510068
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2015-11-29 15:39:33 +0700

    複数のスクリプト追加

---
+++ Commons/Scripts/TransformExtensions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TransformExtensions
+{
+	public static void SetLayer(this Transform trans, int layer) 
+	{
+		trans.gameObject.layer = layer;
+		foreach(Transform child in trans)
+			child.SetLayer( layer);
+	}
+
+	public static void SetCollision(this Transform trans, bool col) 
+	{
+		Collider[] cs = trans.GetComponentsInChildren<Collider> ();
+		foreach (Collider c in cs)
+			c.enabled = col;
+
+		Collider cc = trans.GetComponent<Collider>();
+		if(cc)
+			cc.enabled = col;
+	}
+
+	public static void CopyTo(this Transform trans, Transform dest)
+	{
+		dest.position = trans.position;
+		dest.rotation = trans.rotation;
+		dest.localScale = trans.localScale;
+	}
+}
